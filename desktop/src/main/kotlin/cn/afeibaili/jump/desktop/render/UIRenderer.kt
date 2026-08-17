250abce5cc7213d31d9c4f2908ee05576b5f11a1
Afei <afeibaili@outlook.com>
2026-08-17 13:06:16 +0800

    setting设置提前，修改为属性注入

--- desktop/src/main/kotlin/cn/afeibaili/jump/desktop/render/UIRenderer.kt
+++ desktop/src/main/kotlin/cn/afeibaili/jump/desktop/render/UIRenderer.kt
@@ -3,10 +3,6 @@ package cn.afeibaili.jump.desktop.render
 import cn.afeibaili.gl.render.LayoutRenderer
 import cn.afeibaili.gl.render.RectangleRenderer
 import cn.afeibaili.gl.render.camera.Camera
-import cn.afeibaili.gl.render.layout.fixed.fixed
-import cn.afeibaili.gl.render.layout.shape.rectangle
-import cn.afeibaili.gl.render.layout.weigth.columnWeight
-import cn.afeibaili.gl.render.layout.weigth.rowWeight
 import cn.afeibaili.gl.render.shader.Program
 import cn.afeibaili.gl.render.shader.Shader
 import cn.afeibaili.jump.common.resource.ResourceFileGetter
@@ -26,38 +22,16 @@ class UIRenderer {
     lateinit var layoutRenderer: LayoutRenderer
 
     fun layout() = Application.screen.layout {
-        rowWeight {
-            columnWeight {
-                rectangle("rect2").setting {
-                    it.setWeight(1f).setOffsetX(0f)
-                }
-                rectangle("rect3").setting {
-                    it.setWeight(1f).setOffsetY(20f)
-                }
-            }.setting {
-                it.setWeight(1f)
-            }
-
-            rectangle("rect1").setting {
-                it.setWeight(1f).setOffsetX(0f)
-            }
-        }.setting { it.setSize(400f, 400f) }
-    }.layout {
-        fixed {
-            rectangle("rect4").setting { it.fillMaxSize() }
-        }.setting { it.setSize(400f, 400f).setOffsetY(400f) }
     }
 
     fun init() {
         layout()
         logger.info("loaded layout")
         val vertexShader = Shader.create(
-            Shader.ShaderType.VERTEX,
-            ResourceFileGetter.getResourceFile("shader/rectangle.vert").readText()
+            Shader.ShaderType.VERTEX, ResourceFileGetter.getResourceFile("shader/rectangle.vert").readText()
         )
         val fragmentShader = Shader.create(
-            Shader.ShaderType.FRAGMENT,
-            ResourceFileGetter.getResourceFile("shader/rectangle.frag").readText()
+            Shader.ShaderType.FRAGMENT, ResourceFileGetter.getResourceFile("shader/rectangle.frag").readText()
         )
         val program: Program = Program.create(vertexShader, fragmentShader).apply { link() }
         val camera = Camera(program).apply { ortho(0f, window.width.toFloat(), window.height.toFloat(), 0f, -1f, 1f) }
